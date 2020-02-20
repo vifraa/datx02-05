be2275b687092cdcae7831f7e5b9cb9e3e1a3bdb
ostcarl <ostcarl@student.chalmers.se>
2020-02-20 23:12:30 +0100

    created code (commented for now) to use with Individual and change ages to birthdates

--- simulator/generator.py
+++ simulator/generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import names
+import datetime
 from absl import flags
 from absl import app
 #from individual import Individual
@@ -12,9 +13,15 @@ flags.DEFINE_integer("bpm",100,"Mean of bench press max")
 flags.DEFINE_integer("bpv",5,"Variance in bench press max")
 flags.DEFINE_integer("am",30,"Age mean")
 flags.DEFINE_integer("av",5,"Age variance")
+flags.DEFINE_float("gr", 0.5, "Gender ratio of male")
 
+def gender_to_string(x):
+    if x==0:
+        return "male"
+    elif x==1:
+        return "female"
 
-def generate_indviduals(num, age_mean, age_variance, bench_press_fitness_mean, bench_press_fitness_variance):
+def generate_indviduals(num, age_mean, age_variance, bench_press_fitness_mean, bench_press_fitness_variance, gender_ratio):
     '''
     Generates individuals to be used in the simulator
 
@@ -26,12 +33,27 @@ def generate_indviduals(num, age_mean, age_variance, bench_press_fitness_mean, b
     :return: A list of individuals generated
     '''
 
+    #Holds all individuals
     individuals = []
+    
+    #Normally distributed ages used to create birth dates
     ages = np.random.normal(age_mean, age_variance, num).astype("int")
+    now = datetime.datetime.now()
+    birth_dates = [datetime.datetime(now.year-age, now.month, now.day) for age in ages]
+    
+    #Normally distributed bench press fitnesses used to create bench press movements
     bench_press_fitnesses = np.random.normal(bench_press_fitness_mean, bench_press_fitness_variance, num).astype("int")
+
+    genders = np.ones(num)
+    genders[:int(num*gender_ratio)] = 0
+    np.random.shuffle(genders)  
     for i in range(num):
-        name = names.get_full_name()
-        individuals.append([name, ages[i], bench_press_fitnesses[i]])
+        name = names.get_full_name(gender=gender_to_string(genders[i]))
+        #bench_press_movement = Movement(bench_press_fitnesses[i])
+        #individual = Individual(birth_dates[i], genders[i], name, bench_press_movement)
+        #individuals.append(individual)
+        individuals.append([name, birth_dates[i], bench_press_fitnesses[i], genders[i]])
+
     return individuals
 
 def save_individuals(individuals):
@@ -41,10 +63,10 @@ def save_individuals(individuals):
     pass
 
 def main(argv):
-    generated_individuals = generate_indviduals(FLAGS.n, FLAGS.am, FLAGS.av, FLAGS.bpm, FLAGS.bpv)
+    generated_individuals = generate_indviduals(FLAGS.n, FLAGS.am, FLAGS.av, FLAGS.bpm, FLAGS.bpv, FLAGS.gr)
+    print(generated_individuals)
     #save_individuals()
 
 if __name__ == "__main__":
     app.run(main)
     #print(generate_indviduals(50,30,5,100,5))
-
