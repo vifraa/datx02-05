8db2b3644819deb6e622e6678c81f1839895b62d
Daniel Berg Thomsen <danielbergthomsen@gmail.com>
2020-02-24 14:28:20 +0100

    add pytest structure

---
+++ simulator/tests/test_gym.py
@@ -0,0 +1,27 @@
+import pytest
+from individual import Individual
+
+
+@pytest.fixture()
+def individual():
+    '''Fixture for sample individual with set parameters'''
+    return Individual(id=0, birth=, gender=1, name="Juliana Maddox", weight=40, bench_press_movement=20)
+
+def test_apply_banister():
+    #memes
+
+
+def test_generate_trimp():
+    #memes
+
+
+def test_can_do():
+    #memes
+
+
+def test_load_training():
+    #memes
+
+
+def test_train():
+    #memes
