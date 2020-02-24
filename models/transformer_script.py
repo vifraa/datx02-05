bf4dc8c4bbff61924206767d5a7f8848f5e03dc1
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-02-24 18:31:43 +0100

    commented the transormer script

--- models/transformer_script.py
+++ models/transformer_script.py
@@ -2,6 +2,9 @@
 # This script transforms the data of the generated people and
 # the logs to a shape that can be used by the regression-models
 # ------------------------------------------------------------------------------------
+
+# loads people data from individuals.csv and logs data from training.csv
+# and offer several utility methods.
 class data_loader:
 
     people = []
@@ -31,6 +34,10 @@ class data_loader:
 
 
 # ---------------------------------------------------------------------------------------
+
+# transform the data of the people, the logs and the final performance to one array in the following form
+# [reps1, weight1, reps2, weight2, ..... , reps90, weight90, age, gender, person's_weight, performance]
+# for 90 logs, its size is 2 * 90 + 3 + 1 = 184
 class concatenated_data:
     def concatenate(self):
         return 0
@@ -42,6 +49,11 @@ class concatenated_data:
 
 
 # ---------------------------------------------------------------------------------------
+
+# transform the data of the people and the logs to one array where each log data is squashed
+# with the same personal data, using PCA and dimensional reduction so per1 (R) log1 = s1
+# [s1, s2, s3, ..... , s90, performance] (the array is concatenated with performance)
+# for 90 logs, its size is 90 + 1 = 91
 class squashed_data:
 
     # Fake Data to test the reduction
@@ -70,6 +82,14 @@ class squashed_data:
 
 
 # ---------------------------------------------------------------------------------------
+
+# transform the data of the people and the logs to one array where regarding to the order of the log
+# everything is concatenated in the following form
+# [5*3, 5*5, 5*8, 5*12, 5*15, ...... , 300*3, 300*5, 300*8, 300*12, 300*15   ,age, gender, person's_weight, performance]
+# where 5 is the first possible lifted-weight that a person can have lifted, and 3,5,8,12,15 is the possible reps
+# the values of this array will be the count of times that a person in his/her log have done this training
+# for example [0, 1, 0, 0, 4, ...... , 0, 0, 0, 0, 0,  32, 1, 73, 56.9]
+# Its size is fixed to 290 * 5 + 3 + 1 = 1454
 class detailed_data:
     def detail(self):
         return 0
