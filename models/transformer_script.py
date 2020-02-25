f1d477b54d9c00d4593f58329c23fa6015403d6e
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-02-26 00:56:54 +0100

    did data utility class to manipulate the generated data and didthe concatenated_data class to prepare the data in a specific format to be fed to the regression model

--- models/transformer_script.py
+++ models/transformer_script.py
@@ -1,3 +1,7 @@
+import csv
+
+import pandas as pd
+import numpy as np
 
 # This script transforms the data of the generated people and
 # the logs to a shape that can be used by the regression-models
@@ -5,45 +9,83 @@
 
 # loads people data from individuals.csv and logs data from training.csv
 # and offer several utility methods.
-class data_loader:
+class data_browser:
 
-    people = []
-    logs = []
+    def __init__(self):
+        self.people = np.asarray(pd.read_csv('individuals.csv', sep=','))
+        self.logs = np.asarray(pd.read_csv('training.csv', sep=','))
+        self.log_90_people = []
+        self.LOGS_PER_PERSON = 90
 
-
-    def __init__(self, people_csv, log_csv):
-        return 0
-
-    def load_people_data(self):
-        return 0
+    def get_90_of_age_weight_gender_log_data_for_all_people(self):
+        return self.people[:, 1:4]
 
     def get_90_log_data_for_person(self, id):
-        return 0
+        j = 0
+        tmp = []
+        for r in range(len(self.logs)):
+            if self.logs[r][0] == id:
+                if j < self.LOGS_PER_PERSON:
+                    tmp.append(self.logs[r])
+                    self.log_90_people.append(self.logs[r])
+                    j = j + 1
+                else:
+                    break
+        return tmp
 
-    def get_all_log_data_for_person(self, id):
-        return 0
-
-    def get_all_log_data_for_all(self):
-        return 0
-
+    def get_90_log_data_for_all(self):
+        for i in range(0, 100):
+            self.get_90_log_data_for_person(i)
+        return self.log_90_people
 
 
+    def get_90_log_of_reps_weight_for_all_people(self):
+        self.get_90_log_data_for_all()
+        tmp = np.asarray(self.log_90_people)
+        return tmp[:, 2:4]
 
-
-
-
-
+all = np.asarray(data_browser().get_90_log_of_reps_weight_for_all_people())
+print(all)
 # ---------------------------------------------------------------------------------------
 
 # transform the data of the people, the logs and the final performance to one array in the following form
-# [reps1, weight1, reps2, weight2, ..... , reps90, weight90, age, gender, person's_weight, performance]
+# [reps1, weight1, reps2, weight2, ..... , reps90, weight90, age, person's_weight, gender, performance]
 # for 90 logs, its size is 2 * 90 + 3 + 1 = 184
 class concatenated_data:
-    def concatenate(self):
-        return 0
+    def concatenate_90_logs_for_one_person(self, id):
+        all = np.asarray(data_browser().get_90_log_of_reps_weight_for_all_people())
+        res = []
+        start = id*90
+        end = start + 90
+        for i in range(start, end):
+            res.extend(all[i])
+        return res
+
+    def concatenate_90_logs_for_one_person_with_personal_info_and_performance(self, id):
+        all_logs = np.asarray(self.concatenate_90_logs_for_one_person(id))
+        all_logs_with_performance = np.asarray(data_browser().get_90_log_data_for_person(id))
+        performance = all_logs_with_performance[89][5]
+        personal_info = np.asarray(data_browser().get_90_of_age_weight_gender_log_data_for_all_people())[id]
+        res = []
+        res.extend(all_logs)
+        res.extend(personal_info)
+        res.extend(np.asarray([performance]))
+        return res
 
 
+    def concatenate_90_logs_for_all_with_personal_info_and_performance(self):
+        res = []
+        for i in range(0, 99):
+            res.append(np.asarray(self.concatenate_90_logs_for_one_person_with_personal_info_and_performance(i)))
+        return res
 
+
+con = np.asarray(concatenated_data().concatenate_90_logs_for_all_with_personal_info_and_performance())
+
+with open("regression_dataframes.csv", "w", newline="") as file:
+    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for v in con:
+        wr.writerow(list(v))
 
 
 
@@ -93,3 +135,9 @@ class squashed_data:
 class detailed_data:
     def detail(self):
         return 0
+
+
+
+
+
+
