3af98383c29fca7c8e43a2cdd4a63ea8b4aaa68b
ostcarl <ostcarl@student.chalmers.se>
2020-02-26 18:35:29 +0100

    Add test for generation and gender_to_string(), does not test normal distributed values as of yet

---
+++ simulator/tests/test_generator.py
@@ -0,0 +1,38 @@
+import pytest
+from generator import generate_indviduals, gender_to_string, save_individuals
+import datetime
+import numpy as np
+
+
+def gender_ratio_calc(individuals):
+    """
+    Helper function to get gender_ratio from a list of individuals
+    """
+    __tracebackhide__ = True
+    n_male = 0
+    for i in individuals:
+        if i.gender == 0:
+            n_male += 1
+    return n_male/len(individuals)
+
+
+def test_gender_to_string():
+    """Tests the gender_to_string() method of generator"""
+    assert gender_to_string(1) == "female"
+    assert gender_to_string(0) == "male"
+    assert gender_to_string(2) == "unknown"
+
+
+def test_generate_individuals():
+    """Tests if generated individuals are of the same length and gender_ratio, have not yet figured out how to test the variance and mean"""
+    num = 10
+    age_mean = 25
+    age_variance = 2
+    bench_press_fitness_mean = 100
+    bench_press_fitness_variance = 5
+    gender_ratio = 0.5
+    generated_individuals = generate_indviduals(num,age_mean,age_variance,bench_press_fitness_mean,bench_press_fitness_variance,gender_ratio)
+    
+    assert len(generated_individuals) == num
+    assert gender_ratio == gender_ratio_calc(generated_individuals)
+
