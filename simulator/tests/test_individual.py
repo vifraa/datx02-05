8cf97d2119ffe26f10ae96af0ebd425695e4f984
henriklagergren <henriklagergren@hotmail.com>
2020-02-27 15:56:19 +0100

    Test update travis

--- simulator/tests/test_individual.py
+++ simulator/tests/test_individual.py
@@ -7,6 +7,7 @@ def test_get_age(individual):
     individual.birth = datetime.datetime(datetime.datetime.now().year - 23,1,1)
     assert individual.get_age() == 23
 
+
 def test_to_dataframe(individual):
     df = individual.to_dataframe()
     ind_df = Individual(df)
