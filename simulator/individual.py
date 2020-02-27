88537812e747ee6825e02b519f373b890644f3b7
henriklagergren <henriklagergren@hotmail.com>
2020-02-27 13:50:19 +0100

    Added tests

--- simulator/individual.py
+++ simulator/individual.py
@@ -6,8 +6,8 @@ import pickle
 
 class Individual:
 
-    def __init__(self, id="", birth: datetime = datetime.date(1, 1, 1), gender=2, name="", weight = 0, bench_press_movement=0, path_to_csv=""):
-        if(path_to_csv == ""):
+    def __init__(self, id="", birth: datetime = datetime.date(1, 1, 1), gender=2, name="", weight = 0, bench_press_movement=0, dataframe=None):
+        if(dataframe == None):
             self.birth = birth
             self.gender = gender
             self.name = name
@@ -15,18 +15,18 @@ class Individual:
             self.id = id
             self.bench_press_movement = bench_press_movement
         else:
-            self.load_CSV(path_to_csv)
+            self.load_from_dataframe(dataframe)
 
     def getAge(self, date: datetime):
         return math.floor((date - self.birth).days/365)
 
-    def load_CSV(self, path_to_csv):
-        df = pd.read_csv(path_to_csv)
-        dfRow = df.loc[df['id'] == self.id]
-        self.birth = datetime.strptime(dfRow['birth'],'%Y-%m-%d')
-        self.gender = dfRow['gender']
-        self.name = dfRow['name']
-        self.weight = dfRow['weight']
+    def load_from_dataframe(self, df):
+        self.id = df['id']
+        self.birth = datetime.strptime(df['birth'],'%Y-%m-%d')
+        self.gender = df['gender']
+        self.name = df['name']
+        self.weight = df['weight']
+        self.bench_press_movement = df['bench_press_movement']
 
 
     def to_dataframe(self):
--- simulator/tests/test_individual.py
+++ simulator/tests/test_individual.py
@@ -1,12 +1,18 @@
 import pytest
 from individual import Individual
 import datetime
+import pandas as pd
 
 
 @pytest.fixture()
 def individual():
     '''Fixture for sample individual with set parameters'''
     return Individual(id=0, birth= datetime.datetime(1995,2,3), gender=1, name="Juliana Maddox", weight=40, bench_press_movement=20)
+
+@pytest.fixture()
+def individual_df(df):
+    '''Fixture for sample individual from dataframe'''
+    return Individual(dataframe=df)
 
 
 def test_get_age():
@@ -15,4 +21,7 @@ def test_get_age():
     assert ind.get_age == 23
 
 def test_to_dataframe():
-    #memes
+    ind = individual()
+    df = ind.to_dataframe()
+    ind_df = individual_df(df)
+    assert ind == ind_df
