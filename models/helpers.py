540ec4f53dd2a4bb232771fe930b0a1f2b718e69
Viktor Franzén <viktor.franzen@gung.se>
2020-03-04 14:36:27 +0100

    Helper functions to be used in notebooks.

---
+++ models/helpers.py
@@ -0,0 +1,6 @@
+def print_mean_squared_error(value):
+    print('Mean squared error: %.2f' % value)
+    
+def print_coefficient_of_determination(value):
+    """The coefficient of determination: 1 is perfect prediction"""
+    print('Coefficient of determination: %.2f' % value)
