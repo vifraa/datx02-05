3fd137e6f6c8360571716ca3e00c4e20915196cf
Viktor Franzen <viktor_franzen@hotmail.com>
2020-03-05 19:38:40 +0100

    Cli subcommand example.

--- recengine/cli.py
+++ recengine/cli.py
@@ -1,6 +1,15 @@
 import click
 
-@click.command()
+@click.group()
 def cli():
-    """Example script."""
+    """Recengine is the CLI interface for working with the training recommendation engine.
+    """
+
+
+@cli.command()
+def hello():
+    """Says hello to the world."""
     click.echo("Hello World")
+
+
+
