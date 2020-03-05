e619ed0089e6eaae093b274d3321f6202fde0fce
Viktor Franzen <viktor_franzen@hotmail.com>
2020-03-05 20:11:49 +0100

    Recommend training command with options.

--- recengine/cli.py
+++ recengine/cli.py
@@ -1,4 +1,5 @@
 import click
+from recengine import recommend_training
 
 @click.group()
 def cli():
@@ -12,4 +13,15 @@ def hello():
     click.echo("Hello World")
 
 
-
+@cli.command()
+@click.option('--age', '-a', prompt=True, required=True, type=int)
+@click.option('--weight', '-w', prompt=True, required=True, type=int)
+@click.option('--performance', '-p', prompt=True, required=True, type=float)
+@click.option('--sex', type=click.Choice(['MAN', 'WOMAN'], case_sensitive=False),
+              prompt=True, required=True)
+def recommend(age, weight, performance, sex):
+    """
+    Recommends training based on the given parameters.
+    """
+    
+    click.echo("Worked")
