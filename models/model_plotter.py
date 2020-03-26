e026378ff30ea1277cd3c2612659204a8f9ff96d
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-03-26 07:25:09 +0100

    extended pca_plot

--- models/model_plotter.py
+++ models/model_plotter.py
@@ -1,9 +1,31 @@
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def pca_plot(df, feat_cols):
+
+def pca_plot(df):
     pca = PCA(n_components=3)
-    pca_result = pca.fit_transform(df[feat_cols].values)
-    df['pca-one'] = pca_result[:,0]
-    df['pca-two'] = pca_result[:,1]
-    df['pca-three'] = pca_result[:,2]
+    Xtest_plot = df.copy()
+    pca_result = pca.fit_transform(Xtest_plot.values)
+    Xtest_plot['pca-one'] = pca_result[:, 0]
+    Xtest_plot['pca-two'] = pca_result[:, 1]
+    Xtest_plot['pca-three'] = pca_result[:, 2]
+
     print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
+
+    plt.figure(figsize=(16, 10))
+    sns.scatterplot(
+        x="pca-one", y="pca-two",
+        data=Xtest_plot.loc[:, :]
+    )
+
+    '''
+    sns.scatterplot(
+        x="pca-one", y="pca-two",
+        hue="Ytest",
+        palette=sns.color_palette("hls", 10),
+        data=Xtest_plot.loc[:,:],
+        legend="full",
+        alpha=0.3
+    )
+    '''
