dfae6cbe99623429c2e5a2c816f4893e0919f4e7
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-03-29 01:56:19 +0100

    Added Random Forest Model

---
+++ models/MLmodels/RandomForest.py
@@ -0,0 +1,29 @@
+from MLmodels.DataReader import DataSample
+from sklearn.metrics import mean_squared_error, r2_score
+from helpers import print_mean_squared_error, print_coefficient_of_determination
+from sklearn.model_selection import ShuffleSplit
+from visualizers.model_learning_curve_plotter import Learning_curve_plotter
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import warnings
+
+
+class RandomForest:
+    def __init__(self, data=None):
+        if data is None:
+            self.data = DataSample()
+        else:
+            self.data = data
+        self.regression()
+
+    def regression(self):
+        RandomForestM = RandomForestRegressor(max_depth=10, random_state=0)
+        RandomForestM.fit(self.data.Xtrain, self.data.Ytrain)
+
+        RandomForest_Ypred = RandomForestM.predict(self.data.Xtest)
+
+        RandomForest_mean_squared_error = mean_squared_error(self.data.Ytest, RandomForest_Ypred)
+        RandomForest_r2_score = r2_score(self.data.Ytest, RandomForest_Ypred)
+
+        print_mean_squared_error(RandomForest_mean_squared_error)
+        print_coefficient_of_determination(RandomForest_r2_score)
