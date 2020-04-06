9f97900d58b28eaeb5bb5b9093c46f6960753342
Viktor Franzén <viktor.franzen@gung.se>
2020-04-06 15:22:16 +0200

    Generate arbitrary date string when testing function.

--- recengine/tests/test_data_parser.py
+++ recengine/tests/test_data_parser.py
@@ -1,5 +1,5 @@
-import pytest
 from datetime import datetime
+import pytest
 from hypothesis import given
 from hypothesis.strategies import floats, integers, sampled_from, booleans, fixed_dictionaries, lists, dates
 
@@ -16,18 +16,21 @@ MAPPING = {
     'exercise': integers(min_value=0),
     'weight': floats(min_value=0, allow_infinity=False),
     'repetitions': integers(min_value=0),
-    #'timestamp': datetime.strptime(dates(), "%m/%d/%Y %H:%M")
-    'timestamp': sampled_from(['04/30/2020 04:04'])
+    'timestamp': dates(min_value=datetime(1000, 1, 1).date()).map(
+        lambda x: datetime.strftime(x, "%m/%d/%Y %H:%M"))
 }
 @given(lists(fixed_dictionaries(MAPPING)))
 def test_split_into_weeks(input_sets):
+    """
+    Tests the split_into_weeks function.
+    """
     date_format = "%m/%d/%Y %H:%M"
     weeks = split_into_weeks(input_sets, date_format)
 
     # Assertion holds for now since we have same date on all.
-    assert len(weeks.keys()) <= 1
 
     for week, sets in weeks.items():
         for s in sets:
+            print(s.get('timestamp'))
             set_week = datetime.strptime(s.get('timestamp'), date_format).isocalendar()[1]
             assert week == str(set_week)
