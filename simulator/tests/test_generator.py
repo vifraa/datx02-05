4136f2810c7fff2b42ca8e1da63937081a3fe8ef
Carl Östling <ostcarl@student.chalmers.se>
2020-04-09 17:40:45 +0200

    removed generate individuals because deprecated functionality, will need a new one later

--- simulator/tests/test_generator.py
+++ simulator/tests/test_generator.py
@@ -19,26 +19,6 @@ def test_gender_to_string():
     """Tests the gender_to_string() method of generator"""
     assert gender_to_string(1) == "female"
     assert gender_to_string(0) == "male"
-
-
-def test_generate_individuals():
-    """Tests if generated individuals are of the same length and gender_ratio, have not yet figured
-    out how to test the variance and mean"""
-    num = 10
-    age_mean = 25
-    age_variance = 2
-    bench_press_fitness_mean = 100
-    bench_press_fitness_variance = 5
-    gender_ratio = 0.5
-    weight_mean = 70
-    weight_variance = 5
-    generated_individuals = generate_individuals(
-        num, age_mean, age_variance,
-        weight_mean, weight_variance,
-        bench_press_fitness_mean, bench_press_fitness_variance,
-        gender_ratio)
-
-    assert len(generated_individuals) == num
     assert gender_ratio == gender_ratio_calc(generated_individuals)
 
 
