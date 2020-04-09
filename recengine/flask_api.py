78cf0780a2a24dbbeef5858125d439bbaf94efad
Viktor Franzén <viktor.franzen@gung.se>
2020-04-09 15:14:41 +0200

    Use ttr recengine in web interface and web api.

--- recengine/flask_api.py
+++ recengine/flask_api.py
@@ -50,22 +50,24 @@ def formpbar():
 
 @app.route("/formttr", methods=["POST"])
 def formttr():
-    #recengine = RecommendationEngine("ttr")
     name = request.form.get("fname")
     file = request.files["ffile"]
     stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
-    ttr = ttrdata_from_csv_bytes(stream, "%m/%d/%Y %H:%M")
-    four_weeks_ttrdata = ttr[-8:]
-    print(ttr)
 
-    #best_pred, _ = recengine.recommend_training(data)
-    #program = fetch_program_from_model(best_pred["model"])
+    # TODO Let user input format.
+    ttrdata = ttrdata_from_csv_bytes(stream, "%m/%d/%Y %H:%M")
+    four_weeks_ttrdata = ttrdata[-8:]
+
+    data = np.array(four_weeks_ttrdata).reshape(1, -1)
+    recengine = RecommendationEngine("ttr")
+
+    best_pred, _ = recengine.recommend_training(data)
+    program = fetch_program_from_model(best_pred["model"])
     return render_template("ttr.html",
-                           #best_pred=best_pred["model"].name,
-                           #predicted_performance=best_pred["predicted_performance"],
-                           #program=program,
-                           ttr=ttr,
-                           ttr4= four_weeks_ttrdata,
+                           best_pred=best_pred["model"].name,
+                           predicted_performance=best_pred["predicted_performance"],
+                           program=program,
+                           performance=four_weeks_ttrdata[-1],
                            name=name)
 
 
@@ -101,25 +103,28 @@ def pbar():
 
 @app.route('/api/v1/ttr', methods=['POST'])
 def ttr():
-
-    #recengine = RecommendationEngine("ttr")
-    #best_pred, all_predictions = recengine.recommend_training(data)
-    #program = fetch_program_from_model(best_pred["model"])
-
     sets = request.json.get('sets', [])
-    time_format = request.json.get('timeFormat', '')
+    time_format = request.json.get('timeformat', '')
 
     weeks = split_into_weeks(sets, time_format)
+    if len(weeks.keys()) < 4:
+        return jsonify(error=400, text="Sets spanning atleast four weeks is required.")
+
+
     ttr_d = calculate_ttrdata_from_week_dict(weeks)
     four_weeks_ttrdata = ttr_d[-8:]
 
+    data = np.array(four_weeks_ttrdata).reshape(1, -1)
+
+    recengine = RecommendationEngine("ttr")
+    best_pred, _ = recengine.recommend_training(data)
+    program = fetch_program_from_model(best_pred["model"])
 
     res = {
-        #"predicted_performance": best_pred["predicted_performance"],
-        #"training_program": best_pred["model"].name,
-        #"program_structure": program
-        "ttr": ttr_d,
-        "ttr4": four_weeks_ttrdata
+        "predicted_performance": best_pred["predicted_performance"],
+        "training_program": best_pred["model"].name,
+        "program_structure": program,
+        "calculated_current_1rm": four_weeks_ttrdata[-1]
     }
 
     return jsonify(res)
