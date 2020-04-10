689978e9cc344a77c1eb411503adc66051327ba5
Ljubo Cvijetic <ljubo12@hotmail.com>
2020-04-10 14:42:28 +0200

    navbar added

---
+++ models/webapp/modelssite/src/Components/navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">Models</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Rubrik1</Nav.Link>
+                        <Nav.Link href="#features">Rubrik2</Nav.Link>
+                        <Nav.Link href="#pricing">Rubrik3</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default navbar;
