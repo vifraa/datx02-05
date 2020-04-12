9b3265b884307155bbf16fbf08c7f2dc7959e95c
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-04-12 14:18:53 +0200

    added more structure to app.js

--- models/webapp/modelssite/src/App.js
+++ models/webapp/modelssite/src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
 import './App.css';
-import Navbar from './Components/navbar.jsx'
+import Navbar from './Components/navbar.jsx';
+import main from './Components/main'
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <React.Fragment>
+      <header>Models Admin Site</header>
+
+        <Container fluid={true} style={{ paddingLeft: 0, paddingRight: 0 }}>
+          <Navbar/>
+        </Container>
+
+        <Container className="h-50" fluid={true} style={{ paddingLeft: '10%', paddingRight: 0, marginTop: '10%' }}>
+          <Row>
+            <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={main} />
+                {/*<Route path="/" component={} />*/}
+                <Route component={Default} />
+              </Switch>
+            </BrowserRouter>
+          </Row>
+        </Container>
+
+        <Container fluid={true} style={{ paddingLeft: 0, paddingRight: 0,  marginTop: '10%'}}>
+          <BrowserRouter>
+            <Footer />
+          </BrowserRouter>
+        </Container>
+
+      </React.Fragment>
     </div>
   );
 }
