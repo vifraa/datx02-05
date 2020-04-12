faeaeb594c8703d03365d6e16e0b4bccbef1997d
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-04-12 16:49:09 +0200

    created state fore model card container

--- models/webapp/modelssite/src/Components/modelCardsContainer.jsx
+++ models/webapp/modelssite/src/Components/modelCardsContainer.jsx
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 export default class modelCardsContainer extends Component {
 
     
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
     render() {
         return (
             <div>
