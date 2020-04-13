1be97fc3cf9ff19a9e834ecdce2957e259d9c79d
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-04-13 03:06:18 +0200

    more styling - the uploading func

--- models/webapp/modelssite/src/Components/uploadData.jsx
+++ models/webapp/modelssite/src/Components/uploadData.jsx
@@ -4,16 +4,19 @@ import "../CSS_Folder/uploadData.css";
 class uploadData extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container pt-3 flex">
         <div className="row">
           <div className="col-md-4">
             <form method="post" action="#" id="#">
               <div className="form-group files">
-                <label>Upload Your File </label>
                 <input type="file" className="form-control" multiple="" />
               </div>
             </form>
           </div>
+          <div className="col-md-4 m-auto">
+            <label>You can choose to upload your csv file here or to run on the default simulated individuals medium data set </label>
+          </div>
+
         </div>
       </div>
     );  
