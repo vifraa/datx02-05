5cfca9cfcb4eab083365decd8f880623dbfa3fcd
Viktor Franzen <viktor_franzen@hotmail.com>
2020-04-15 16:43:11 +0200

    Test that the calculation of rest is correct.

--- recengine/tests/test_recengine.py
+++ recengine/tests/test_recengine.py
@@ -3,10 +3,12 @@ import tempfile
 from hypothesis import given
 from hypothesis.strategies import floats, integers, sampled_from, booleans
 import os
+import csv
+import math
 import datetime
 
 from model import Model
-from recengine import fetch_program_from_model
+from recengine import fetch_program_from_model, _create_program_from_csv_reader
 
 
 def test_fetch_program_bad_input():
@@ -41,3 +43,19 @@ def test_fetch_program_from_model():
 
             today = p_set.date
             assert previous_set_date == today
+
+def test_correctly_calculated_rest():
+    """
+    Tests that the calculated rest is correct.
+    """
+    program_path = os.path.join(os.path.dirname(__file__), "training_programs", "ogasawara_HL.csv")
+
+    with open(program_path, newline='') as file:
+        program_reader = csv.reader(file, delimiter="|")
+        _headers = next(program_reader)
+
+        program = _create_program_from_csv_reader(program_reader)
+        for _day, sets in program.items():
+            for p_set in sets:
+                assert isinstance(p_set.rest, float)
+                assert math.isclose(p_set.rest, 3.0)
