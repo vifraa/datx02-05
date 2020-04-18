6ddba9366c2f0fdcc885a5e87261423271c674c7
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-04-18 05:44:53 +0200

    creating simulator sections

--- webapp/src/Components/SimulatorComponents/simulator_main.jsx
+++ webapp/src/Components/SimulatorComponents/simulator_main.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import generateIndividuals from './generateIndividuals';
+import trainIndividuals from './trainIndividuals';
 
 export default class simulator_main extends Component {
     render() {
         return (
             <div>
-                
+                <div id="simulator_body">
+                    <div id="GenerateIndividuals">
+                        <generateIndividuals />
+                    </div>
+                    <div id="TrainIndividuals">
+                        <trainIndividuals />
+                    </div>
+                </div>
             </div>
         )
     }
