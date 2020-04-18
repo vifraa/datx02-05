757b016719d1d57d884a81b8ea4c454fa50a0cf2
YazanGhafir <yazan.yasser.ghafir@gmail.com>
2020-04-18 05:45:44 +0200

    added trainIndividuals component

---
+++ webapp/src/Components/SimulatorComponents/trainIndividuals.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class trainIndividuals extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
